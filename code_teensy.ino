24a7fd822a86e29299b467bb82a0cadeead8c050
Pol2901 <paul2901@orange.fr>
2025-05-08 20:21:38 +0200

    Update code_teensy.ino

--- code_teensy.ino
+++ code_teensy.ino
@@ -1,4 +1,5 @@
 float q0, q1, q2, q3;
+float q0_filt,q1_filt,q2_filt,q3_filt;
 bool justCameBackFromZero = false;
 String inputLine = "";
 
@@ -24,7 +25,20 @@ void loop() {
 
       float a, b, c1, d;
       if (sscanf(inputLine.c_str(), "%f %f %f %f", &a, &b, &c1, &d) == 4) {
-        q0 = a; q1 = b; q2 = c1; q3 = d;
+        q0_filt = a; q1_filt = b; q2_filt = c1; q3_filt = d;
+
+        float norm = sqrt(q0_filt*q0_filt
+                + q1_filt*q1_filt
+                + q2_filt*q2_filt
+                + q3_filt*q3_filt);
+        
+        q0 = q0_filt / norm;
+        q1 = q1_filt / norm;
+        q2 = q2_filt / norm;
+        q3 = q3_filt / norm;
+
+
+        
 
         bool isZeroFrame = (q0 == 0.0 && q1 == 0.0 && q2 == 0.0 && q3 == 0.0);
 
@@ -36,10 +50,10 @@ void loop() {
           justCameBackFromZero = false;
         } else {
           Serial.print("Quaternion: ");
-          Serial.print(q0); Serial.print(", ");
-          Serial.print(q1); Serial.print(", ");
-          Serial.print(q2); Serial.print(", ");
-          Serial.println(q3);
+          Serial.print(q0,6); Serial.print(", ");
+          Serial.print(q1,6); Serial.print(", ");
+          Serial.print(q2,6); Serial.print(", ");
+          Serial.println(q3,6);
         }
       } else {
         Serial.println("[WARN] Malformed frame ignored: " + inputLine);
@@ -51,5 +65,8 @@ void loop() {
     }
   }
 }
+
+
+
 
 
